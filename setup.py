e47ac1ef13affbe101f7993cefcbdf8b2d2d950f
Thomas Kolar <thomaskolar90@gmail.com>
2021-10-06 20:43:56 +0200

    pre-pypi prettification

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='Sweeping View',
-    description='Readers for Minesweeper replays',
+    name="sweeping-view",
+    description="Readers for Minesweeper replays",
+    long_description_content_type="text/markdown",
     license="MIT",
-    author='Thomas Kolar',
-    author_email='thomaskolar90@gmail.com',
-    packages=['sweeping_view'],
+    author="Thomas Kolar",
+    author_email="thomaskolar90@gmail.com",
+    url="https://github.com/ralokt/sweeping-view/",
+    packages=["sweeping_view"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Other Audience",
+        "Topic :: Games/Entertainment :: Puzzle Games",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "minesweeper",
+        "replay",
+        "readers",
+    ],
 )
