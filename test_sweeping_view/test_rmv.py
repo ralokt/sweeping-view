f224e9603e232fdb44b975950bbb638072d69d6a
Thomas Kolar <thomaskolar90@gmail.com>
2025-02-17 13:57:43 +0100

    add test for rmv2

--- test_sweeping_view/test_rmv.py
+++ test_sweeping_view/test_rmv.py
@@ -20,3 +20,31 @@ def test_rmv(replay_path):
         (6, 6),
         (7, 6),
     }
+
+
+def test_rmv2(replay_path):
+    rmv = RMVReplay.from_file(replay_path / "test_subject_2.rmv")
+
+    assert rmv.clone_id == 1
+    assert rmv.major_version_of_clone == 5
+    assert rmv.format_version == 2
+    assert rmv.player_data["name"] == "Thomas Kolar"
+    assert rmv.player_data["nickname"] == "ralokt"
+    assert rmv.properties["level"] == "beginner"
+    assert rmv.extension_properties["vsweep_skin_fname"] == b"24px_new.bmp"
+    assert set(rmv.mines) == {
+        (0, 2),
+        (0, 6),
+        (1, 5),
+        (2, 3),
+        (3, 6),
+        (5, 2),
+        (5, 3),
+        (6, 0),
+        (6, 1),
+        (6, 6),
+    }
+    assert rmv.events[-2:] == [
+        {'col': 7, 'row': 7, 'subtype': 'open_1', 'type': 'board'},
+        {'how': 'win', 'type': 'terminate'},
+    ]
